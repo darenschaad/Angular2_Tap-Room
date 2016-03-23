471b4cd9b4e2ef163183750d9e2647d834cf877a
Brad London and Daren Schaad <darenschaad@gmail.com>
2016-03-23 15:12:59 -0700

    Add a decrementing button to sell a pint

---
+++ app/done.pipe.ts
@@ -0,0 +1,11 @@
+// import {Pipe, PipeTransform} from 'angular2/core';
+// import {Keg} from './keg.model';
+//
+// @Pile({
+//
+// })
+// export class DonePipe implements PipeTransform {
+//   transform(input: Task[], args) {
+//     var desiredDoneState = args[0];
+//   }
+// }
--- app/keg.component.ts
+++ app/keg.component.ts
@@ -7,9 +7,14 @@ import { Keg } from './keg.model';
   inputs: ['keg'],
   template: `
     <h3>{{keg.beerName}} $\{{keg.price}}, ABV= {{keg.abv}}%, Brewery: {{keg.beerBrand}}, Pints Remaining: {{keg.pintCount}}</h3>
+    <button class="btn-sm btn-info" (click)="beerSale(currentKeg)">Sell Pint</button>
   `
 })
 
 export class KegComponent {
   public keg: Keg;
+  beerSale(clickedKeg: Keg): void {
+    this.keg.pintCount = this.keg.pintCount-1 ;
+
+  }
 }
